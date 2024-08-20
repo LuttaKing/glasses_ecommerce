236d887338dcc49a0c2700cf6ad19f1702df3ff3
LuttaKing <denilsonwashuma@gmail.com>
2024-08-20 10:46:07 +0300

    Initial Commit

---
+++ glasses_ecommerce/glasses_ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  glasses_ecommerceApp.swift
+//  glasses_ecommerce
+//
+//  Created by Denilson Washuma on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct glasses_ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
